6faa90089f0f013ddbbfa40232eeed719f200b2b
Sebastian <128631661+menita2005@users.noreply.github.com>
2024-04-07 13:09:33 -0500

    feat(navegacion): se agrego componente PrimaryButton, settings y about

---
+++ navegacion/src/presentation/screens/about/AboutScreen.tsx
@@ -0,0 +1,11 @@
+import {Text, View} from 'react-native';
+
+const AboutScreen = () => {
+    return (
+        <View>
+            <Text>about</Text>
+        </View>
+    )
+}
+
+export default AboutScreen;
